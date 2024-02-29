72f5eeb0c2af314421b3ffd42befb40c7b189780
elv-haoyu <haoyu.zhang@eluv.io>
2024-02-28 16:53:51 -0800

    Add pointwise loss fucntion w/o running the script

--- experiments/run_hpo.py
+++ experiments/run_hpo.py
@@ -8,6 +8,8 @@ from hyperopt import fmin, tpe, hp
 import argparse
 from sklearn.cluster import KMeans
 import pandas as pd
+from loguru import logger
+from collections import defaultdict
 
 
 from src.index import FaissIndex
@@ -18,22 +20,58 @@ from src.rank import SimpleRanker
 from src.query_understanding import SimpleQueryProcessor
 from src.format import SearchArgs, SearchOutput
 from src.utils import LRUSearchCache
+import src.loss as LossFunc
 
-# TODO: implement
-def get_score(res: SearchOutput, data: pd.DataFrame) -> float:
-    return np.random.rand()
+
+def get_loss(res: SearchOutput, data: pd.DataFrame, query, k=20, reRank=False, useRank=True) -> float:
+    _df = data.loc[data['query'] == query]
+    iqShot2rating = {k: v for k, v in zip(_df['iqShot'], _df['avg_rating'])}
+    ranking, simiScore, rating = [], [], []
+    newTopM = defaultdict(list)
+    if 'results' not in res:
+        logger.info('Cannot pass search results')
+        return None
+    for rs in res['results']:
+        if len(ranking) >= k:
+            break
+        try:
+            _iqShot = rs['hash'] + '|' + str(rs['fields']['f_start_time'][-1]) + '_' + str(
+                rs['fields']['f_end_time'][-1])
+            _rank = rs['rank']
+            _score = rs['score']
+        except KeyError as e:
+            logger.info('search results missing key', e)
+        else:
+            if _iqShot in iqShot2rating:
+                ranking.append(_rank)
+                simiScore.append(_score)
+                rating.append(iqShot2rating[_iqShot])
+            newTopM[_iqShot].append({
+                'query': query,
+                'rank': _rank,
+                'similarity score': _score,
+            })
+    # TODO: where to dump the newTopM
+    y_true = np.array(rating)
+    pred_score = np.array(simiScore)
+    pred_rank = np.array(ranking)
+    return LossFunc.pointwise_regression_error(y_true, pred_score, pred_rank, topk=k, reRank=reRank, useRank=useRank)
+
 
 def _get_num_docs(content_id: str, client: ElvClient) -> int:
-    res = client.content_object_metadata(object_id=content_id, metadata_subtree='indexer/stats/document/total')
+    res = client.content_object_metadata(
+        object_id=content_id, metadata_subtree='indexer/stats/document/total')
     return int(res)
 
 # converts uids of format iq_start_time_end_time into uid for fabric search which is based on hash + doc-prefix
+
+
 def convert(uids: List[str], client: ElvClient, content_id: str) -> List[str]:
     ids = set(uid.split("|")[0] for uid in uids)
     # get num docs
     num_docs = _get_num_docs(content_id, client)
     args = {
-        #"terms": "",
+        # "terms": "",
         "ids": ",".join(ids),
         "max_total": "-1",
         "limit": str(num_docs),
@@ -41,17 +79,23 @@ def convert(uids: List[str], client: ElvClient, content_id: str) -> List[str]:
     }
     res = client.search(object_id=content_id, query=args)
     start_end_times = [uid.split("|")[1].split('_') for uid in uids]
-    start_end_times = [(int(start), int(end)) for start, end in start_end_times]
-    uids = [doc["hash"]+doc["prefix"] for doc in res["results"] if (doc["fields"]["f_start_time"][0], doc["fields"]["f_end_time"][0]) in start_end_times]
+    start_end_times = [
+        (int(start), int(end)) for start, end in start_end_times]
+    uids = [doc["hash"]+doc["prefix"] for doc in res["results"]
+            if (doc["fields"]["f_start_time"][0], doc["fields"]["f_end_time"][0]) in start_end_times]
     return uids
 
 # Returns a function which evaluates a given set of parameters. We want to optimize this function.
+
+
 def get_evaluation(qid: str, auth: str, encoder: TextEncoder, data: pd.DataFrame) -> Callable[[dict], float]:
     path = os.path.join(config.DATA_PATH, "experiments")
-    # initialize an elv-client where calls to the search api are cached for future use. This is helpful for building the index, 
+    # initialize an elv-client where calls to the search api are cached for future use. This is helpful for building the index,
     # where we need to make a single large request to the search api to get all the documents
-    client = LRUSearchCache(ElvClient.from_configuration_url(config.CONFIG_URL, auth))
+    client = LRUSearchCache(
+        ElvClient.from_configuration_url(config.CONFIG_URL, auth))
     processor = SimpleQueryProcessor(client, encoder)
+
     def run_experiment(params) -> float:
         T = params["T"]
         K = int(params["K"])
@@ -59,6 +103,7 @@ def get_evaluation(qid: str, auth: str, encoder: TextEncoder, data: pd.DataFrame
         index = FaissIndex(tmp_path, config.IndexConstructor)
         # takes in 2d numpy array and removes rows with similarity greater than T with another row
         # also uses k means to reduce the number of rows to K centroids
+
         def postprocess(x: np.ndarray) -> np.ndarray:
             to_remove = []
             for i in range(x.shape[0]):
@@ -71,12 +116,13 @@ def get_evaluation(qid: str, auth: str, encoder: TextEncoder, data: pd.DataFrame
                 kmeans.fit(x)
                 x = kmeans.cluster_centers_
             return x
-    
+
         index_buider = update.IndexBuilder(encoder, postprocess)
         index_buider.build(qid, index, client)
         index.set_path(os.path.join(path, qid, f"{T}_{K}"))
         ranker = SimpleRanker(index)
-        searcher = SimpleSearcher(index_qid=qid, client=client, processor=processor, index=index, ranker=ranker)
+        searcher = SimpleSearcher(
+            index_qid=qid, client=client, processor=processor, index=index, ranker=ranker)
         total = 0
         queries = data["query"].unique()
         for q in queries:
@@ -84,17 +130,18 @@ def get_evaluation(qid: str, auth: str, encoder: TextEncoder, data: pd.DataFrame
             uids = convert(uids, client, qid)
             args = {
                 "search_fields": "f_object,f_speech_to_text,f_logo,f_celebrity,f_segment,f_display_title",
-                "max_total": len(uids), 
+                "max_total": len(uids),
                 "limit": len(uids),
-                "filters": ' '.join(f"uid:\"{uid}\"" for uid in uids), 
+                "filters": ' '.join(f"uid:\"{uid}\"" for uid in uids),
                 "terms": q
             }
             args = SearchArgs().load(args)
             res = searcher.search(args)
-            total += get_score(res, data)
+            total += get_loss(res, data, q)
 
         return total / len(queries)
     return run_experiment
+
 
 def main():
     space = {
@@ -105,15 +152,20 @@ def main():
     encoder = get_encoder_with_cache(config.SBERT_MODEL)
     data = pd.read_csv(args.data)
     evaluator = get_evaluation(args.qid, args.auth, encoder, data)
-    
-    best = fmin(fn=evaluator, space=space, algo=tpe.suggest, max_evals=args.samples)
+
+    best = fmin(fn=evaluator, space=space,
+                algo=tpe.suggest, max_evals=args.samples)
     print(best)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--auth", type=str, help="Authorization token", required=True)
+    parser.add_argument("--auth", type=str,
+                        help="Authorization token", required=True)
     parser.add_argument("--qid", type=str, help="Index id", required=True)
-    parser.add_argument("--samples", type=int, help="Number of samples to try", required=True)
-    parser.add_argument("--data", type=str, help="Path to csv file containing queries and their scores for evaluating the model", required=True)
+    parser.add_argument("--samples", type=int,
+                        help="Number of samples to try", required=True)
+    parser.add_argument(
+        "--data", type=str, help="Path to csv file containing queries and their scores for evaluating the model", required=True)
     args = parser.parse_args()
     main()
