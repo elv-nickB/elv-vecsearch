65b065e34a01d2e5742a19fadfcbbd1392071532
elv-nickB <nick.buoncristiani@eluv.io>
2024-04-04 17:36:53 +0000

    mirroring fabric search endpoint

--- src/update/builder.py
+++ src/update/builder.py
@@ -10,7 +10,6 @@ import src.config as config
 from src.index.faiss import Index
 from src.embedding.abstract import DocEncoder
 from src.utils import timeit
-
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class IndexBuilder():
@@ -30,58 +29,58 @@ class IndexBuilder():
         self.ended_tasks = {}
         self.lock = threading.Lock()
 
-    def stop(self, qid: str, status: str="stopped") -> UpdateStatus:
+    def stop(self, hash: str, status: str="stopped") -> UpdateStatus:
         with self.lock:
-            if qid not in self.running_tasks:
+            if hash not in self.running_tasks:
                 return None
-            task = self.running_tasks[qid]
+            task = self.running_tasks[hash]
             task.stop_event.set()
-            shutil.rmtree(os.path.join(config.TMP_PATH, qid), ignore_errors=True)
+            shutil.rmtree(os.path.join(config.TMP_PATH, hash), ignore_errors=True)
             task.status = status
-            self.ended_tasks[qid] = task
-            del self.running_tasks[qid]
+            self.ended_tasks[hash] = task
+            del self.running_tasks[hash]
             return task
 
-    def get_status(self, content_id: str) -> UpdateStatus:
+    def get_status(self, hash: str) -> UpdateStatus:
         with self.lock:
-            if content_id in self.running_tasks:
-                return self.running_tasks[content_id]
-            elif content_id in self.ended_tasks:
-                return self.ended_tasks[content_id]
+            if hash in self.running_tasks:
+                return self.running_tasks[hash]
+            elif hash in self.ended_tasks:
+                return self.ended_tasks[hash]
             else:
                 return None
 
     """
     Args:
-        content_id: index object id to base vector index off of
+        hash: version hash of index object to base vector index off of
 
     Saves a vector index for the given content_id which can be searched.
     """
-    def build(self, content_id: str, index: Index, client: ElvClient) -> None:
+    def build(self, hash: str, index: Index, client: ElvClient) -> None:
         with self.lock:
-            if content_id in self.running_tasks:
-                logging.error(f"Indexing already in progress for {content_id}.")
+            if hash in self.running_tasks:
+                logging.error(f"Indexing already in progress for {hash}.")
                 return
             update_state = self.UpdateStatus('running', 0)
-            self.running_tasks[content_id] = update_state
+            self.running_tasks[hash] = update_state
         try:
-            self._build(content_id, index, client)
-            self.stop(content_id, status="finished")
+            self._build(hash, index, client)
+            self.stop(hash, status="finished")
         except Exception as e:
             with self.lock:
-                task = self.running_tasks[content_id]
+                task = self.running_tasks[hash]
                 task.status = 'error'
                 task.error = str(e.with_traceback(None))
-                self.ended_tasks[content_id] = task
-                del self.running_tasks[content_id]
+                self.ended_tasks[hash] = task
+                del self.running_tasks[hash]
         
-    def _build(self, content_id: str, index: Index, client: ElvClient):
-        update_state = self.running_tasks[content_id]
-        field_configs = self._get_field_configs(content_id, client)
-        num_docs = self._get_num_docs(content_id, client)
+    def _build(self, hash: str, index: Index, client: ElvClient):
+        update_state = self.running_tasks[hash]
+        field_configs = self._get_field_configs(hash, client)
+        num_docs = self._get_num_docs(hash, client)
         # Unpack the entire index with a "select all" query
         with timeit("unpacking index"):
-            all_docs = client.search(object_id=content_id, query={"filters": "has_field:id", "max_total": num_docs, "limit": num_docs, "display_fields": ["all"]})['results']
+            all_docs = client.search(version_hash=hash, query={"filters": "has_field:id", "max_total": num_docs, "limit": num_docs, "display_fields": ["all"]})['results']
         logging.info("Embedding documents and adding to index.")
         for i, doc in enumerate(all_docs):
             if update_state.stop_event.is_set():
@@ -102,17 +101,17 @@ class IndexBuilder():
     def cleanup(self) -> None:
         with self.lock:
             logging.info("Performing clean up...")
-            for qid in self.running_tasks:
-                self.running_tasks[qid].stop_event.set()
-                del self.running_tasks[qid]
+            for hash in self.running_tasks:
+                self.running_tasks[hash].stop_event.set()
+                del self.running_tasks[hash]
             # TODO: this global path might eventually be shared so probably should parameterize it
             shutil.rmtree(config.TMP_PATH, ignore_errors=True)
 
-    def _get_field_configs(self, content_id: str, client: ElvClient) -> Dict[str, Dict[str, float]]:
-        res = client.content_object_metadata(object_id=content_id, metadata_subtree='indexer/config/indexer/arguments/fields')
+    def _get_field_configs(self, hash: str, client: ElvClient) -> Dict[str, Dict[str, float]]:
+        res = client.content_object_metadata(version_hash=hash, metadata_subtree='indexer/config/indexer/arguments/fields')
         return res
 
     # Retrieves the total number of documents in the index
-    def _get_num_docs(self, content_id: str, client: ElvClient) -> int:
-        res = client.content_object_metadata(object_id=content_id, metadata_subtree='indexer/stats/document/total')
+    def _get_num_docs(self, hash: str, client: ElvClient) -> int:
+        res = client.content_object_metadata(version_hash=hash, metadata_subtree='indexer/stats/document/total')
         return int(res)
